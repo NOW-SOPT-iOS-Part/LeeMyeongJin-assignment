f3c497488a72f3033220230dc758648f36b1075e
LeeMyeongJin <dlaudwls1203@gmail.com>
2024-04-07 21:21:00 +0900

    [Feat] #3 - 텍스트 필드 패딩 구현

---
+++ tvingProject/tvingProject/Extension/UITextField+.swift
@@ -0,0 +1,22 @@
+//
+//  UITextField+.swift
+//  tvingProject
+//
+//  Created by 이명진 on 4/7/24.
+//
+
+import UIKit
+
+extension UITextField {
+    func addLeftPadding(width: CGFloat) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: width, height: self.frame.height))
+        self.leftView = paddingView
+        self.leftViewMode = .always
+    }
+    
+    func addRightPadding(width: CGFloat) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: width, height: self.frame.height))
+        self.rightView = paddingView
+        self.rightViewMode = .always
+    }
+}
