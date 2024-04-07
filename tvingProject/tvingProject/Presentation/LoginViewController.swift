54edf2dcef68a271f05e17ed69f2d0d69c1f0dcf
LeeMyeongJin <dlaudwls1203@gmail.com>
2024-04-07 19:00:55 +0900

    [Setting] #3 - 프로젝트 기초 세팅

---
+++ tvingProject/tvingProject/Presentation/LoginViewController.swift
@@ -0,0 +1,49 @@
+//
+//  LoginViewController.swift
+//  tvingProject
+//
+//  Created by 이명진 on 4/7/24.
+//
+
+import UIKit
+
+final class LoginViewController: UIViewController {
+    
+    // MARK: - UIComponents
+    
+    // MARK: - Life Cycles
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setUI()
+        setHierarchy()
+        setLayout()
+        setDelegate()
+        setRegister()
+    }
+    
+    
+    // MARK: - UI & Layout
+    
+    private func setUI() {
+        view.backgroundColor = .green
+    }
+    
+    private func setHierarchy() {
+        
+    }
+    
+    private func setLayout() {
+        
+    }
+    
+    private func setDelegate() {
+        
+    }
+    
+    private func setRegister() {
+        
+    }
+    
+}
