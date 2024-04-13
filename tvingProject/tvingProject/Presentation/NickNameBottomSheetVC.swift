382d58a9e3021c551f27c951d9eff2044b196b47
LeeMyeongJin <dlaudwls1203@gmail.com>
2024-04-13 18:40:01 +0900

    [Add] #3 - NickNameBottomSheetVC 파일 추가

---
+++ tvingProject/tvingProject/Presentation/NickNameBottomSheetVC.swift
@@ -0,0 +1,49 @@
+//
+//  NickNameBottomSheetVC.swift
+//  tvingProject
+//
+//  Created by 이명진 on 4/13/24.
+//
+
+import UIKit
+
+final class NickNameBottomSheetVC: UIViewController {
+    
+    // MARK: - UIComponents
+    
+    // MARK: - Life Cycles
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setUI()
+        setHierarchy()
+        setLayout()
+        setDelegate()
+        setRegister()
+    }
+    
+    
+    // MARK: - UI & Layout
+    
+    private func setUI() {
+        view.backgroundColor = .white
+    }
+    
+    private func setHierarchy() {
+        
+    }
+    
+    private func setLayout() {
+        
+    }
+    
+    private func setDelegate() {
+        
+    }
+    
+    private func setRegister() {
+        
+    }
+    
+}
