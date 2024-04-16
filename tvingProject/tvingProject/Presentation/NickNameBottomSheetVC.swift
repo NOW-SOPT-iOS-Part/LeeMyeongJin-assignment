cf56c3d711cda139328a416937178892fa0c65e0
LeeMyeongJin <dlaudwls1203@gmail.com>
2024-04-16 18:26:21 +0900

    [Fix] #3 - 바텀 시트 프로토콜 데이터 전달 로직

--- tvingProject/tvingProject/Presentation/NickNameBottomSheetVC.swift
+++ tvingProject/tvingProject/Presentation/NickNameBottomSheetVC.swift
@@ -10,25 +10,38 @@ import UIKit
 import SnapKit
 import Then
 
+protocol LoginViewControllerProtocol: AnyObject {
+    func dataBind(nickName: String)
+}
+
 final class NickNameBottomSheetVC: UIViewController {
     
-    
-    // MARK: - Property
+    weak var delegate: LoginViewControllerProtocol?
+    // MARK: - UIComponents
     
     private let nickName = UILabel().then {
         $0.text = "닉네임을 입력해 주세요"
+        $0.font = .pretendardFont(weight: 500, size: 23)
     }
     
     private lazy var nicknameTextField = UITextField().then {
         $0.placeholder = "닉네임을 입력해주세요 최대 (10자)"
+        $0.layer.cornerRadius = 3
+        $0.backgroundColor = .gray1
+        $0.addLeftPadding(width: 25)
     }
     
     private lazy var saveButton = UIButton().then {
         $0.setTitle("저장하기", for: .normal)
         $0.titleLabel?.font = .pretendardFont(weight: 600, size: 14)
+        $0.backgroundColor = .gray
+        $0.layer.cornerRadius = 12
+        $0.isEnabled = false
+        $0.setTitleColor(.white, for: .selected)
+        $0.addTarget(self, action: #selector(saveButtonTapped), for: .touchUpInside)
     }
     
-    // MARK: - UIComponents
+    private var userNickName: String = ""
     
     // MARK: - Life Cycles
     
@@ -37,7 +50,7 @@ final class NickNameBottomSheetVC: UIViewController {
         modalPresentationStyle = .custom
         transitioningDelegate = self
     }
-
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
@@ -48,8 +61,8 @@ final class NickNameBottomSheetVC: UIViewController {
         setUI()
         setHierarchy()
         setLayout()
+        setDelegate()
     }
-    
     
     // MARK: - UI & Layout
     
@@ -70,16 +83,56 @@ final class NickNameBottomSheetVC: UIViewController {
             $0.top.equalToSuperview().offset(50)
             $0.leading.equalToSuperview().offset(20)
         }
+        
+        nicknameTextField.snp.makeConstraints {
+            $0.top.equalTo(nickName.snp.bottom).offset(21)
+            $0.leading.trailing.equalToSuperview().inset(20)
+            $0.height.equalTo(52)
+        }
+        
+        saveButton.snp.makeConstraints {
+            $0.bottom.equalToSuperview().inset(20)
+            $0.leading.trailing.equalToSuperview().inset(20)
+            $0.height.equalTo(52)
+        }
     }
     
-    func setData(nickName: String) {
-        self.nickName.text = nickName
+    private func setDelegate() {
+        nicknameTextField.delegate = self
+    }
+    
+    // MARK: - Methods
+    
+    @objc
+    private func saveButtonTapped() {
+        delegate?.dataBind(nickName: self.userNickName)
+        self.dismiss(animated: true, completion: nil)
     }
     
 }
 
+// MARK: - UIViewControllerTransitioningDelegate
+
 extension NickNameBottomSheetVC: UIViewControllerTransitioningDelegate {
     func presentationController(forPresented presented: UIViewController, presenting: UIViewController?, source: UIViewController) -> UIPresentationController? {
         return HalfSizePresentationController(presentedViewController: presented, presenting: presenting)
+    }
+}
+
+
+// MARK: - UITextFieldDelegate
+
+extension NickNameBottomSheetVC: UITextFieldDelegate {
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+        if let text = textField.text, text.count > 0 {
+            saveButton.isEnabled = true
+            saveButton.backgroundColor = .tvingRed
+            
+            userNickName = text
+            print(userNickName)
+        } else {
+            saveButton.isEnabled = false
+            saveButton.backgroundColor = .gray1
+        }
     }
 }
