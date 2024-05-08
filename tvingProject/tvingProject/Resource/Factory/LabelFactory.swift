f0910f9e66a78f93e3e28a264c4354ec97a61b23
LeeMyeongJin <dlaudwls1203@gmail.com>
2024-05-08 16:22:16 +0900

    [Add] #7 - Label Factory 함수 추가

---
+++ tvingProject/tvingProject/Resource/Factory/LabelFactory.swift
@@ -0,0 +1,27 @@
+//
+//  LabelFactory.swift
+//  tvingProject
+//
+//  Created by 이명진 on 5/8/24.
+//
+
+import UIKit
+
+import Then
+
+struct LabelFactory {
+    static func build(
+        text: String?,
+        font: UIFont,
+        backgroundColor: UIColor = .clear,
+        textColor: UIColor = .black,
+        textAlignment: NSTextAlignment = .center) -> UILabel {
+            return UILabel().then {
+                $0.text = text
+                $0.font = font
+                $0.backgroundColor = backgroundColor
+                $0.textColor = textColor
+                $0.textAlignment = textAlignment
+            }
+        }
+}
